f0322bb4c5d464466ff0bd363a396a3c0830e843
louannecostes <louanne.costes@gmail.com>
2023-02-23 17:11:19 +0100

    ajout des requetes

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,14 @@
 FROM node:latest
+#pull the js image from docker hub
 RUN mkdir /usr/src/app/
-COPY package.json /usr/src/app/
+#create the usr, src, and app dirs in the container 
+COPY ./api/ /usr/src/app/
+#copy the content of the /api folder on the host machine to the /usr/src/app/ folder in the container 
 WORKDIR /usr/src/app/
-EXPOSE 3000
+#set the workdir to /usr/src/app/
+EXPOSE 3333
+#expose the port 5000 of the container
 RUN npm install
+#install all the python libraries listed in requirements.txt
 CMD ["npm", "start"]
+#run the command : npm start
---
+++ api/index.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const sql = require('mysql')
+const app = express();
+const PORT = 3333;
+const conn = sql.createConnection({
+    host: 'mariadb', 
+    user:'root', 
+    password: 'root',
+    database : 'mydatabase_node'
+});
+
+conn.connect(function(err) {
+    if (err) throw err;
+    console.log("You are connected!");
+  });
+
+app.get('/customers', (req, res) =>{    
+    conn.query('select * from customers', (err, results) => {
+        if(err) throw err
+        res.end(JSON.stringify(results)); // Result in JSON format
+    });
+})
+    
+
+app.get("/customers/:id", (req, res) => {
+    const id = req.params.id;
+
+    conn.query('SELECT * FROM customers WHERE id ='+id, (err, results)=>{
+        if(err) throw err
+        res.end(JSON.stringify(results)); // Result in JSON format
+    });
+})
+
+app.delete("/customers/:id", (req, res) => {
+    const id = req.params.id;
+
+    conn.query('delete FROM customers WHERE id ='+id, (err, results)=>{
+        if(err) throw err
+        res.end(JSON.stringify(results)); // Result in JSON format
+    });
+})
+
+app.post("/customers/", (req, res) => {
+    const firstname = req.query.firstname;
+    const name = req.query.name;
+    const email = req.query.email;
+
+
+    conn.query(`Insert into customers (firstname, name, email) values ('`+firstname+`','`+name+`','`+email+`')`, (err, results)=>{
+        if(err) throw err
+        res.end(JSON.stringify(results)); // Result in JSON format
+    });
+})
+
+
+app.put("/customers/:id", (req, res) => {
+    const id = req.params.id;
+
+    const firstname = req.query.firstname;
+    const name = req.query.name;
+    const email = req.query.email;
+
+
+    conn.query(`Update customers SET firstname ='`+firstname+`', name ='`+name+`', email ='`+email+`' where id = `+id, (err, results)=>{
+        if(err) throw err
+        res.end(JSON.stringify(results)); // Result in JSON format
+    });
+})
+
+
+
+
+app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
